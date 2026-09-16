891c75da627018e42b00a98de97ebef8f02f918e
Yahya CHAKIRI <yahyachakiridev@gmail.com>
2026-09-16 08:24:46 +0100

    feat(i18n): translate management frontend routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { ToastProvider } from '@ury/ui'
 import './index.css'
-import App from './App.tsx'
 
+import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react'
+import { ToastProvider } from '@ury/ui'
+import { createRoot } from 'react-dom/client'
+import { initFrontendI18n } from './i18n'
+
+initFrontendI18n()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter basename="/ury">
